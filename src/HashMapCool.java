5a359caef8fdda39340c8cf6dbfb0db7dc72787a
Tonmoy Roy <tonmoy18@gmail.com>
2015-11-03 22:01:45 -0500

    Implement LockFreeList

--- src/HashMapCool.java
+++ src/HashMapCool.java
@@ -1,15 +1,21 @@
 
+class Entry {
+	int val;
+	public Entry(int v) {
+		val = v;
+	}
+}
+
 public class HashMapCool {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		CoarseHashSet<Integer> hashTable = new CoarseHashSet<Integer>(25);
-		hashTable.add(5);
-		
-		System.out.print(hashTable.contains(5));
+		LockFreeList<Integer> l = new LockFreeList<Integer>();
+	 	System.out.println(l.add(1));
+	 	System.out.println(l.contains(1));
+	 	System.out.println(l.remove(1));
+	 	System.out.println(l.contains(1));
 	}
-	
-
 }
--- src/LockFreeList.java
+++ src/LockFreeList.java
@@ -2,92 +2,109 @@ import java.util.concurrent.atomic.*;
 
 public class LockFreeList<T> {
 
-  public class Node {
-    public int key;
-    public AtomicMarkableReference<Node> next;
-    T item;
-    Node(T item) {
-      this.item = item;
-    }
-  }
+	public class Node {
+		public int key;
+		public AtomicMarkableReference<Node> next;
+		T item;
 
-  class Window {
-    public Node pred, curr;
-    Window(Node p, Node c) {
-      pred = p;
-      curr = c;
-    }
-    public Window find(Node head, int key) {
-      Node pred = null, curr = null, succ = null;
-      boolean[] marked = {false};
-      boolean snip;
-      retry: while(true) {
-        pred = head;
-        curr = pred.next.getReference();
-        while(true) {
-          succ = curr.next.getReference();
-          while (marked[0]) {
-            snip = pred.next.compareAndSet(curr, succ, false, false);
-            if (!snip) continue retry;
-            curr = succ;
-            succ = curr.next.get(marked);
-          }
-          if (curr.key >= key)
-            return new Window(pred, curr);
-          pred = curr;
-          curr = succ;
-        }
-      }
-    }
+		Node(T item) {
+			this.item = item;
+			if (item != null) key = item.hashCode();
+		}
+	}
 
-    public boolean add(T item) {
-      int key = item.hashCode();
-      while(true) {
-        Window window = find(head, key);
-        Node pred = window.pred, curr = window.curr;
-        if (curr.key == key) {
-          return false;
-        }
-        else {
-          Node node = new Node(item);
-          node.next = new AtomicMarkableReference(curr, false);
-          if (pred.next.compareAndSet(curr, node, false, false)) {
-            return true;
-          }
-        }
-      }
-    }
+	Node head;
 
-    public boolean remove(T item) {
-      int key = item.hashCode();
-      boolean snip;
-      while (true) {
-        Window window = find(head, key);
-        Node pred = window.pred, curr = window.curr;
-        if (curr.key != key) {
-          return false;
-        }
-        else {
-          Node succ = curr.next.getReference();
-          snip = curr.next.compareAndSet(succ, succ, false, false);
-          if (!snip)
-            continue;
-          pred.next.compareAndSet(curr, succ, false, false);
-          return true;
-        }
-      }
-    }
+	public LockFreeList() {
+		head = new Node(null);
+		Node next = new Node(null);
+		Node nextNext = new Node(null);
+		head.key = Integer.MIN_VALUE;
+		next.key = Integer.MAX_VALUE;
+		nextNext.key = Integer.MAX_VALUE;
+		
+		next.next = new AtomicMarkableReference<Node>(nextNext, false);
+		head.next = new AtomicMarkableReference<Node>(next, false);
+	}
 
-    public boolean contains(T item) {
-      boolean[] marked = {false};
-      int key = item.hashCode();
-      Node curr = head;
-      while (curr.key < key) {
-        curr = curr.next.getReference();
-        Node succ = curr.next.get(marked);
-      }
-      return (curr.key == key && !marked[0]);
-    }
-  }
+	class Window {
+		public Node pred, curr;
+
+		Window(Node p, Node c) {
+			pred = p;
+			curr = c;
+		}
+	}
+
+	public Window find(Node head, int key) {
+		Node pred = null, curr = null, succ = null;
+		boolean[] marked = { false };
+		boolean snip;
+		retry: while (true) {
+			pred = head;
+			curr = pred.next.getReference();
+			while (true) {
+				succ = curr.next.getReference();
+				while (marked[0]) {
+					snip = pred.next.compareAndSet(curr, succ, false, false);
+					if (!snip)
+						continue retry;
+					curr = succ;
+					succ = curr.next.get(marked);
+				}
+				if (curr.key >= key)
+					return new Window(pred, curr);
+				pred = curr;
+				curr = succ;
+			}
+		}
+	}
+
+	public boolean add(T item) {
+		int key = item.hashCode();
+		while (true) {
+			Window window = find(head, key);
+			Node pred = window.pred, curr = window.curr;
+			if (curr.key == key) {
+				return false;
+			} else {
+				Node node = new Node(item);
+				node.next = new AtomicMarkableReference<Node>(curr, false);
+				if (pred.next.compareAndSet(curr, node, false, false)) {
+					return true;
+				}
+			}
+		}
+	}
+
+	public boolean remove(T item) {
+		int key = item.hashCode();
+		boolean snip;
+		while (true) {
+			Window window = find(head, key);
+			Node pred = window.pred, curr = window.curr;
+			if (curr.key != key) {
+				return false;
+			} else {
+				Node succ = curr.next.getReference();
+				snip = curr.next.compareAndSet(succ, succ, false, false);
+				if (!snip)
+					continue;
+				pred.next.compareAndSet(curr, succ, false, false);
+				return true;
+			}
+		}
+	}
+
+	public boolean contains(T item) {
+		boolean[] marked = { false };
+		int key = item.hashCode();
+		Node curr = this.head;
+		while (curr.key < key) {
+			curr = curr.next.get(marked);
+		}
+		return (curr.key == key && !marked[0]);
+	}
+
 
 }
