3ade11ffb5b0bf9d0e8e6264c2c9e989970d4d00
Tonmoy Roy <tonmoy18@gmail.com>
2015-11-25 18:24:11 -0500

    Add framework for benchmark

--- src/hashcool/HashMapCool.java
+++ src/hashcool/HashMapCool.java
@@ -9,5 +9,17 @@ public class HashMapCool {
 		
 		System.out.println("This is the main program!");
 		
+		Benchmark benchmark = new Benchmark();
+		
+		benchmark.runBenchmarks();
+		
+		System.out.println("coarseHashSequentialAddTime= " + benchmark.coarseHashSequentialAddTime);
+		System.out.println("lockFreeHashSequentialAddTime= " + benchmark.lockFreeHashSequentialAddTime);
+		System.out.println("coarseHashRandomAddTime= " + benchmark.coarseHashRandomAddTime);
+		System.out.println("lockFreeHashRandomAddTime= " + benchmark.lockFreeHashRandomAddTime);
+		System.out.println("coarseHashSequentialRemoveTime= " + benchmark.coarseHashSequentialRemoveTime);
+		System.out.println("lockFreeHashSequentialRemoveTime= " + benchmark.lockFreeHashSequentialRemoveTime);
+		
+		
 	}
 }
